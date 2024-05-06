78c3fb42147a77cab426190cd3890ea42922af55
Jonathan Kaeser <jonathan.kaeser@adnovum.ch>
2024-05-06 15:13:55 +0200

    mock network interaction with msw

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,18 @@
  import '../src/index.css';
 
+ import { initialize, mswDecorator } from 'msw-storybook-addon';
+
+/*
+ * Initializes MSW
+ * See https://github.com/mswjs/msw-storybook-addon#configuring-msw
+ * to learn how to customize it
+ */
+initialize();
+
 //👇 Configures Storybook to log the actions( onArchiveTask and onPinTask ) in the UI.
 /** @type { import('@storybook/react').Preview } */
 const preview = {
+  decorators: [mswDecorator],
   parameters: {
     controls: {
       matchers: {
--- src/components/InboxScreen.stories.jsx
+++ src/components/InboxScreen.stories.jsx
@@ -1,7 +1,8 @@
 
 import InboxScreen from './InboxScreen';
 import store from '../lib/store';
-
+import { http, HttpResponse } from 'msw'
+ import { MockedState } from './TaskList.stories';
 import { Provider } from 'react-redux';
 
 export default {
@@ -11,6 +12,26 @@ export default {
   tags: ['autodocs'],
 };
 
-export const Default = {};
 
-export const Error = {};
+export const Default = {
+    parameters: {
+      msw: [
+        http.get('https://jsonplaceholder.typicode.com/todos?userId=1', () => {
+        // ...and respond to them using this JSON response.
+    return HttpResponse.json(MockedState.tasks)
+  }),
+      ],
+    },
+  };
+
+
+  export const Error = {
+    parameters: {
+      msw: [
+        http.get('https://jsonplaceholder.typicode.com/todos?userId=1', () => {
+        // ...and respond to them using this JSON response.
+    return new HttpResponse(null, { status: 403 })
+  }),
+      ],
+    },
+  };
